3b8e9b0b15e7fc62ea4441f8fc88165e90691b48
JellyBoy <pkkann@gmail.com>
2013-09-30 21:27:22 +0200

    s

---
+++ hism_v2/src/entity/Enrollment.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+/**
+ *
+ * @author patrick
+ */
+@Entity
+@Table(name = "enrollment")
+public class Enrollment implements Serializable {
+    
+    @Id
+    @Column(name = "idenrollment")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idEnrollment;
+    
+    @OneToOne
+    @JoinColumn(name = "idperson")
+    @Cascade(CascadeType.SAVE_UPDATE)
+    private Person enrolledPerson;
+    
+    @OneToOne
+    @JoinColumn(name = "iduser")
+    @Cascade(CascadeType.SAVE_UPDATE)
+    private User enrolledByUser;
+    
+    @OneToMany
+    @Column(name = "guests")
+    private Set<Guest> guests = new HashSet(0);
+
+    public Enrollment() {
+    }
+
+    public Enrollment(Person enrolledPerson, User enrolledByUser) {
+        this.enrolledPerson = enrolledPerson;
+        this.enrolledByUser = enrolledByUser;
+    }
+
+    public int getIdEnrollment() {
+        return idEnrollment;
+    }
+
+    public void setIdEnrollment(int idEnrollment) {
+        this.idEnrollment = idEnrollment;
+    }
+
+    public Person getEnrolledPerson() {
+        return enrolledPerson;
+    }
+
+    public void setEnrolledPerson(Person enrolledPerson) {
+        this.enrolledPerson = enrolledPerson;
+    }
+
+    public User getEnrolledByUser() {
+        return enrolledByUser;
+    }
+
+    public void setEnrolledByUser(User enrolledByUser) {
+        this.enrolledByUser = enrolledByUser;
+    }
+
+    public Set getGuests() {
+        return guests;
+    }
+
+    public void setGuests(Set guests) {
+        this.guests = guests;
+    }
+    
+    
+}
--- hism_v2/src/hism/Hism.java
+++ hism_v2/src/hism/Hism.java
@@ -4,17 +4,26 @@
  */
 package hism;
 
+import hibernate.HiberUtil;
+import org.hibernate.Transaction;
+import org.hibernate.classic.Session;
+
 /**
  *
  * @author patrick
  */
 public class Hism {
-
+    
     public Hism() {
+        
     }
-
+    
     public static void main(String[] args) {
+        Session s = HiberUtil.getSessionFactory().openSession();
+        Transaction tx = s.beginTransaction();
         
-        
+        tx.commit();
+        s.close();
     }
+    
 }
